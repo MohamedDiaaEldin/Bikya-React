9d04f8734ecf3d4e5586369602a36f365c4d2c5b
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-04 23:23:09 +0300

    Refactored HowItWorks     - added main container to the section
    Refactored login button style

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = ()=> {
 
                 <ul className={isOpen ?'display-menu':'' } >
                     <li><a className="active" href="#">Home</a></li>
-                    <li><a href="#services">How It Works</a></li>
+                    <li><a href="#howItWorks">How It Works</a></li>
                     <li><a href="#portfolio">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                     <li><a href="#login"  className='login-btn'>Login</a> </li>
--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -11,45 +11,46 @@ const HowItWorks = ()=> {
 
     console.log('how it works reloaded with ', activeTab, ' state')
     return (  
-        <>
+        < div id='howItWorks'>
             <h2 className='heading'>How It Works</h2>
             <div className='toggle-btns '>
                 <button onClick={()=>clickHandler('buyer') } className={activeTab === 'buyer' ? 'active' : ''}>Buyer</button>
                 <button onClick={()=> clickHandler('seller') } className={activeTab === 'seller' ? 'active' : ''}>Seller</button>
             </div>
 
-            <div className='content-container'>
-      {/* first row */}
-            <div>
-                <ContentBox
-                    header={activeTab === 'seller' ? 'Setup Your Seller Account' : 'Setup Your Buyer Account'}
-                    iconNumber={'1'}
-                    content={activeTab === 'seller' ? 'Provide accurate and complete information to create your seller account.' : 'Enter your details accurately to create your buyer account.'}
-                />
-                <ContentBox
-                    header={'Select Order Information'}
-                    iconNumber={'2'}
-                    content={'Choose the desired martial, weight, and availability for your order.'}
-                />
-            </div>
+            <div className='content-container container'>
 
-            {/* second row */}
-            <div>
-                <ContentBox
-                    header={'Submit Your Order'}
-                    iconNumber={'3'}
-                    content={activeTab === 'seller' ?  "Personalize your order by selecting specific details such as martial type, weight, and your preferred pickup date." : 'Customize your order by selecting specific details such as martial type, weight, and availability. Ensure a seamless experience by allowing location permissions to accurately process your order.' }
-                />
-                <ContentBox
-                    header={activeTab === 'seller' ? 'Earn Points' :  'Receive Your Car and Collect Your Order'}
-                    iconNumber={'4'}
-                    content={activeTab === 'seller' ?  'Visit our stock to collect your materials. Our team will assist you in locating and packaging your items' : 'Every order you place earns you reward points! Accumulate points with each purchase and redeem them for exciting rewards . Your points balance will be updated automatically after each successful transaction.'}
-                />
-            </div>
+                {/* first row */}
+                <div>
+                    <ContentBox
+                        header={activeTab === 'seller' ? 'Setup Your Seller Account' : 'Setup Your Buyer Account'}
+                        iconNumber={'1'}
+                        content={activeTab === 'seller' ? 'Provide accurate and complete information to create your seller account.' : 'Enter your details accurately to create your buyer account.'}
+                    />
+                    <ContentBox
+                        header={'Select Order Information'}
+                        iconNumber={'2'}
+                        content={'Choose the desired martial, weight, and availability for your order.'}
+                    />
+                </div>
+
+                {/* second row */}
+                <div>
+                    <ContentBox
+                        header={'Submit Your Order'}
+                        iconNumber={'3'}
+                        content={activeTab === 'seller' ?  "Personalize your order by selecting specific details such as martial type, weight, and your preferred pickup date." : 'Customize your order by selecting specific details such as martial type, weight, and availability. Ensure a seamless experience by allowing location permissions to accurately process your order.' }
+                    />
+                    <ContentBox
+                        header={activeTab === 'seller' ? 'Earn Points' :  'Receive Your Car and Collect Your Order'}
+                        iconNumber={'4'}
+                        content={activeTab === 'seller' ?  'Visit our stock to collect your materials. Our team will assist you in locating and packaging your items' : 'Every order you place earns you reward points! Accumulate points with each purchase and redeem them for exciting rewards . Your points balance will be updated automatically after each successful transaction.'}
+                    />
+                </div>
 
                 
             </div>
-        </>
+        </div>
     )
 }
 
