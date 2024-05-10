c38ea14792186635d38183a8de215ac8419a04eb
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-10 10:50:30 +0300

    - implemented Login component     - used useSate and useReducer to Handle form validly     - implemented responsive css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,74 @@
+import React, { useReducer, useRef } from 'react';
+import './login.css'; 
+
+const initialState = {
+    isValidPassword: false,
+    isValidUserName: false
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'USERNAME':
+            
+            console.log('payload', action.payload)
+            return { ...state, isValidUserName: action.isValid };
+        case 'PASSWORD':
+            return { ...state, isValidPassword: action.isValid };
+        default:
+            return state;
+    }
+};
+
+function Login() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const userNameRef = useRef('');
+    const passwordRef = useRef('');
 
 
-const Login = () => { 
-    
-    return( 
-        <h2>Login</h2>
-    )
+
+    const isValidForm = state.isValidPassword && state.isValidUserName
+
+    const userNameOnBlurHandler = () => {
+        const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        dispatch({ type: 'USERNAME', isValid: pattern.test(userNameRef.current.value) });
+    };
+
+    const passwordOnBlurHandler = () => {
+        dispatch({ type: 'PASSWORD', isValid: passwordRef.current.value.trim().length > 0 });
+    };
+
+    const handleLogin = () => {
+        // send make http request 
+        // show loading bar 
+        // if 200 : go to user area else show error message
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        onBlur={userNameOnBlurHandler}
+                        ref={userNameRef}
+                        className={!state.isValidUserName ? 'invalid' : ''}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        ref={passwordRef}
+                        className={!state.isValidPassword ? 'invalid' : ''}
+                        onBlur={passwordOnBlurHandler}
+                    />
+                </div>
+                <button disabled={!isValidForm} type="button" onClick={handleLogin}>Login</button>
+            </form>
+        </div>
+    );
 }
 
-
-export default Login
+export default Login;
