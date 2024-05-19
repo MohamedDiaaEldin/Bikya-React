29ad9b5f9fa83e5e80e84412aa9f937d09054191
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-19 11:48:53 +0300

    Feat : Implement Products Component - Comprises Header and ProductList Components

---
+++ src/common/Products.js
@@ -0,0 +1,17 @@
+import Header from "../components/Header"
+import ProductsList from "./ProductsList"
+
+
+
+  
+const Products = ()=> { 
+    return(
+        <section>
+            <Header/>
+            <ProductsList/>
+
+        </section>
+    )
+}
+
+export default Products
