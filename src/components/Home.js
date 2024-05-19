325669f8043f57be00cd26a80ecdb247ca58f18d
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-19 11:52:06 +0300

    Feat : Implement Navigation Functionality for Logged In users - Utilized AuthenticationContextProvider and useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,22 @@ import Landing from '../components/Landing';
 import HowItWorks from '../components/HowItWorks';
 import About from '../components/About';
 import Footer from '../components/Footer';
+import { useAuthAppContext } from '../AuthenticationContextProvider';
+import { useEffect } from 'react';
+import {useNavigate} from 'react-router-dom'
 
 function Home() {
+  const {isLoggedIn} = useAuthAppContext()
+  const navigate = useNavigate()
+
+
+  useEffect(()=> { 
+    if(isLoggedIn){
+        navigate('/main')
+    }
+}, [])
   return (
+    
     <>
       <Header />
       <Landing/>
